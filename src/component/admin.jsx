68f4989fbad5143d5d91a098f467eea3a5ab5e87
VuTrungNghia <vunghia467@gmail.com>
2024-07-27 14:57:09 +0700

    Create Content

--- src/component/admin.jsx
+++ src/component/admin.jsx
@@ -1,11 +1,21 @@
+// src/components/AdminDashboard.jsx
 import React from 'react';
 import Header from './partial/header'; 
 import Footer from './partial/footer'; 
+
 function AdminDashboard() {
+  const Content = {
+        textAlign: 'center',
+        margin: '20px 0',
+        fontSize: '32px',
+        color: '#333',
+        marginLeft: '950px',
+        marginRight: '-800px',
+      };
   return (
     <div>
       <Header />
-      <h1>Hello</h1>
+        <h1 style={Content}>Hello Welcome!. Vu Trung Nghia</h1>
       <Footer />
     </div>
   );
